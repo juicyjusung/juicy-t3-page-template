587ad78266f4b2febb65e9dde265876d16604f3f
juicyjusung <juicyjusung@gmail.com>
2024-01-20 22:25:40 +0900

    init: init project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '~/styles/globals.css';
+
+import { type AppType } from 'next/app';
+
+import { api } from '~/utils/api';
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default api.withTRPC(MyApp);
